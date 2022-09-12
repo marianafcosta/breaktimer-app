865d1485558dc71f85d22f6ea4d14d5f4e30b188
Mariana Costa <mariana.costa214@gmail.com>
2022-09-12 19:34:03 +0200

    chore: increase window size in settings

--- app/main/lib/windows.ts
+++ app/main/lib/windows.ts
@@ -37,8 +37,8 @@ export function createSettingsWindow(): void {
     show: false,
     width: 507,
     minWidth: 507,
-    height: process.platform === "win32" ? 740 : 700,
-    minHeight: process.platform === "win32" ? 740 : 700,
+    height: 850,
+    minHeight: 850,
     autoHideMenuBar: true,
     icon:
       process.env.NODE_ENV === "development"
